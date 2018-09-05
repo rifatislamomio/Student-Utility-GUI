4c402c7bc917df01845c128960f387fe7b57ee6c
Omio <rifatislamomio@gmail.com>
2018-09-06 02:42:22 +0600

    8th

---
+++ Menu/src/menuController.java
@@ -0,0 +1,48 @@
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+
+
+public class menuController {
+
+    public void exit(ActionEvent e)
+    {
+        Platform.exit();
+    }
+
+    public void about(ActionEvent e)
+    {
+
+    }
+
+    public void home(ActionEvent e)
+    {
+
+    }
+
+    public void userInfo(ActionEvent e)
+    {
+
+    }
+
+    public void messenger(ActionEvent e)
+    {
+
+    }
+
+    public void calculator(ActionEvent e)
+    {
+
+    }
+
+    public void note(ActionEvent e)
+    {
+
+    }
+
+    public void timer(ActionEvent e)
+    {
+
+    }
+
+
+}
